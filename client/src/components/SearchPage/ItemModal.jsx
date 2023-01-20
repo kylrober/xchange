92ee11c7feea39eaf836692f9ebbd1d4c0257f55
Kyle Robers <kyl.rober@gmail.com>
2023-01-19 21:36:27 -0700

    implemented a modal to display item details when a pin is clicked on map

---
+++ client/src/components/SearchPage/ItemModal.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import axios from 'axios';
+import './modal.css';
+
+const { useState } = React;
+
+function ItemModal({showDetails, setShowDetails, onCloseItem, item}) {
+  if (!showDetails) {
+    return null;
+  }
+
+  const onClick = () => {
+    setShowDetails(false);
+  }
+
+  const toItem = () => {
+    console.log('send stuff to item');
+
+  }
+
+  return (
+    <div className="modal-background">
+      <div className="item-modal">
+        <div className="modal-header">
+          <div className="titleCloseBtn">
+            <button className="closeBtn" onClick={() => onCloseItem()}> X </button>
+          </div>
+          <h2 className="modal-title">DETAILS</h2>
+        </div>
+        <div className="modal-content" />
+          <div className="modal-break">
+            <a className="info-title">item:</a>
+            <br></br>
+            <a className="modal-text">{item.name}</a>
+          </div>
+          <div className="modal-break">
+            <a className="info-title">description:</a>
+            <br></br>
+            <a className="modal-text"> {item.description}</a>
+          </div>
+          <div className="modal-break">
+            <a className="info-title">condition:</a>
+            <br></br>
+            <a className="modal-text">{item.condition}</a>
+          </div>
+        <div className="modal-body">
+          <button className="modal-button" onClick={() => toItem()}>GO TO ITEM</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ItemModal;
