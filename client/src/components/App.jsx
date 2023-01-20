e08acae1d55781fa4531cf0667736d0a6dd927bb
Kyle Robers <kyl.rober@gmail.com>
2023-01-19 21:39:39 -0700

    updatd app file to take changeView and view.props to allow view switches

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,10 +4,15 @@ import Auth from './Auth';
 import Profile from './ProfilePage';
 import Map1 from './SearchPage/Map';
 import SearchPage from './SearchPage/index.jsx';
+import Item from './ItemDetails/index.jsx';
 
 export default function App() {
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('user') ?? 'null'));
-  const [view, setView] = useState({ name: 'Auth', props: { setUser } });
+  // const [view, setView] = useState({ name: 'Auth', props: { setUser } });
+  // const [view, setView] = useState({ name: 'Profile', props: { setUser } });
+  // const [view, setView] = useState({ name: 'SearchPage', props: { setUser } });
+  const [view, setView] = useState({ name: 'Map', props: { setUser } });
+  // const [view, setView] = useState({ name: 'ItemDetails', props: { setUser } });
 
   const changeView = (viewName, viewProps, isCallback = false) => (
     !isCallback ? setView({ name: viewName, props: viewProps })
@@ -22,10 +27,12 @@ export default function App() {
         break;
       case 'Profile':
         return <Profile changeView={changeView} props={view.props} />;
+      case 'SearchPage':
+        return <SearchPage changeView={changeView} props={view.props} ß/>;
       case 'Map':
-        return <Map1 />;
-      case 'Search':
-        return <SearchPage />
+        return <Map1 changeView={changeView} props={view.props} ß/>;
+      case 'ItemDetails':
+        return <Item changeView={changeView} props={view.props} />;
       default:
         return null;
     }
